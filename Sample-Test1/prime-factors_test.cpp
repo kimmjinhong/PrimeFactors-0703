e544078fcf3157f5829bef554237d95f58ea0632
jinhong <jh0703.kim@gmail.com>
2024-07-03 11:15:59 +0900

    [feature] page 37

--- Sample-Test1/prime-factors_test.cpp
+++ Sample-Test1/prime-factors_test.cpp
@@ -9,3 +9,9 @@ TEST(PrimeFactor, Of1) {
 	vector<int> expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
 }
+
+TEST(PrimeFactor, Of2) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
+}
